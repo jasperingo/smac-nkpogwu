9021587066c304c045f58d567450d155ee411791
Anelechukwu Jasper <jasperanels@gmail.com>
2025-10-14 14:02:55 +0100

    Finished get programs

--- src/app/admin/(portal)/programs/page.tsx
+++ src/app/admin/(portal)/programs/page.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link';
 import MenuList from '@/components/menu-list';
 import SearchForm from '@/components/search-form';
+import GenericTable from '@/components/generic-table';
+import PaginationList from '@/components/pagination-list';
+import { findPrograms } from '@/services/program-service';
+import { resolvePaginationParams } from '@/utils/pagination';
 
 export default async function AdminProgramsPage({ searchParams }: { searchParams: Promise<{ page?: string; search?: string }> }) {
   const { page, search } = await searchParams;
+
+  const programs = await findPrograms(search?.length === 0 ? null : (search ?? null), resolvePaginationParams(page));
 
   return (
     <section className="bg-foreground p-4">
@@ -11,7 +18,26 @@ export default async function AdminProgramsPage({ searchParams }: { searchParams
 
       <SearchForm value={search} action="/admin/programs" placeholder="Search by ID or Name" />
       
+      <GenericTable
+        items={programs.data}
+        headings={[ 'ID', 'Name', 'Theme', 'Topic', 'Action' ]}
+        renderItem={(program) => (
+          <tr key={program.id}>
+            <td className="p-2 border">{ program.id }</td>
+            <td className="p-2 border">{ program.name }</td>
+            <td className="p-2 border">{ program.theme ?? '(Not set)' }</td>
+            <td className="p-2 border">{ program.topic ?? '(Not set)' }</td>
+            <td className="p-2 border">
+              <Link 
+                href={`/admin/programs/${program.id}`}
+                className="text-sm py-1 px-2 bg-primary text-on-primary hover:bg-primary-variant"
+              >Details</Link>
+            </td>
+          </tr>
+        )}
+      />
 
+      <PaginationList path="/admin/programs" pagination={programs} params={new Map([['search', search]])} />
     </section>
   );
 }
--- src/services/program-service.ts
+++ src/services/program-service.ts
@@ -1,8 +1,38 @@
 'use server'
 
+import { eq, like, or } from 'drizzle-orm';
+import { ProgramEntity } from '@/models/entity';
 import { database } from '@/database/connection';
 import { programsTable } from '@/database/schema';
-import { CreateProgramDto } from '@/models/dto';
+import { CreateProgramDto, PaginatedListDto, PaginationDto } from '@/models/dto';
+import { calculatePaginationOffset, calculatePaginationPages } from '@/utils/pagination';
+
+export async function findPrograms(search: string | null, pagination: PaginationDto): Promise<PaginatedListDto<ProgramEntity>> {
+  const where = search === null 
+    ? undefined 
+    : (
+      or(
+        eq(programsTable.name, search),
+        like(programsTable.name, `%${search}%`),
+        isNaN(Number(search)) ? undefined : eq(programsTable.id, Number(search)),
+      )
+    );
+
+  const count = await database.$count(programsTable, where);
+
+  const programs = await database.select()
+    .from(programsTable)
+    .where(where)
+    .limit(pagination.pageLimit)
+    .offset(calculatePaginationOffset(pagination.page, pagination.pageLimit));
+
+  return {
+    data: programs,
+    totalItems: count,
+    currentPage: pagination.page,
+    totalPages: calculatePaginationPages(count, pagination.pageLimit),
+  };
+}
 
 export async function createProgram(dto: CreateProgramDto) {
   const result = await database.insert(programsTable).values({ ...dto }).$returningId();
