f5a28389ed12c3450b4f1254b1fad7caa4338e6e
Anelechukwu Jasper <jasperanels@gmail.com>
2025-10-22 16:09:22 +0100

    Improved breadcrumbs with links and key uniqueness

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Breadcrumbs({ excludeRoot }: Readonly<{ excludeRoot?: boolean; }>) {
   const path = usePathname();
 
   const crumbs = path.substring(1).split('/');
+
+  const root = crumbs[0];
 
   if (excludeRoot) {
     crumbs.shift();
@@ -18,8 +21,10 @@ export default function Breadcrumbs({ excludeRoot }: Readonly<{ excludeRoot?: bo
       <ul className="flex gap-2 items-center flex-wrap">
         { 
           crumbs.map((p, i) => (
-            <li key={p} className="flex gap-2 items-center">
-              <span>{ p.substring(0, 1).toUpperCase().concat(p.substring(1).toLowerCase()).replace('-', ' ') }</span>
+            <li key={p + i} className="flex gap-2 items-center">
+              <Link href={(excludeRoot ? '/' + root : '') + crumbs.slice(0, i + 1).reduce((pv, cv) => pv + '/' + cv, '')}>
+                { p.substring(0, 1).toUpperCase().concat(p.substring(1).toLowerCase()).replace('-', ' ') }
+              </Link>
 
               { i < crumbs.length - 1 && <ChevronRight /> }
             </li>
