fd81aac12715948936f70eb721fc60f94ba4c6a4
Anelechukwu Jasper <jasperanels@gmail.com>
2026-02-07 23:43:54 +0100

    Github action Test 15

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Application entry point for cPanel hosting
  */
 const http = require('http');
-const next = required('next');
+const next = require('next');
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
